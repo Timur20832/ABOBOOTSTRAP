d164e5d0dbf222e8c961063bd225267757419c55
Timur20832 <92998774+Timur20832@users.noreply.github.com>
2021-11-03 13:25:16 +0300

    end

--- js/kod.js
+++ js/kod.js
@@ -1,47 +1,63 @@
-var arr2 = '';
-var arr3 = '';
-var arr4 = '';
-var arr5 = '';
-var arr6 = '';
 document.getElementById('param-1').oninput = function () {
-    // ползунок - длина массива
  document.getElementById('password-length').innerHTML = this.value;
 }
-var 
-result,
-b = document.getElementByName('name1').value,
-y = document.getElementByName('name2').value,
-z = document.getElementByName('name3').value,
-k = document.getElementByName('name4').value ,
-c = document.getElementByName('name5').value,
-m =document.getElementByName('name6').value ;
-
-    document.getElementByName('param-2').onclick = generate();
-    document.getElementById('out').result = '';
-    console.log(result);
-    /*
-    for (var k = 0; k < 6; k++) {
-        var pass = ''; // будущий пароль
-        var passLength = parseInt(document.getElementById('param-1').value); // длина пароля
-        for (var i = 0; i < passLength; i++) {
-            // цикл по длине пароля
-            // выбираю случайное значение из массива result
-            pass += result;
-        }
-        console.log(pass);
-        document.getElementById('out').innerHTML += '<p>' + pass + '</p>';
-    }
-*/
+let result;
+let par1;
+let par2;
+let par3;
+let par4;
+let par5;
 function generate() {
-    result = b+y+z+k+c+m;
-    alert(result);
-}
-function compareRandom(a, b) {
-    return Math.random() - 0.5;
-}
+    const y = document.getElementsByName('name1')[0].value.replace(".");
+    const m = document.getElementsByName('name2')[0].value;
+    const z = document.getElementsByName('name3')[0].value;
+    const k = document.getElementsByName('name4')[0].value;
+    const c = document.getElementsByName('name5')[0].value;
+    var passLength = parseInt(document.getElementById('param-1').value);
+    var rs = "";    var rk = "";  var rj = "";  var ry ="";  var ru = ""; 
+    var lelte =document.getElementById('out').value ;
+    result = y+m+k+z+c;
+    par1 = y+m;
+    par2 = z+m+k;
+    par3 = y+c;
+    par4 = y+m+c;
+    par5 = result;
+   // if (lelte = ''){
+            if (y != "" && m != "" && k != "" && c != "" && z != "") {
+                while (rs.length < passLength) {
+                    rs +=   par1[Math.floor(Math.random() * passLength)];
+                    }
+                //console.log(rs);
+                while (rk.length < passLength) {
+                    rk +=   par2[Math.floor(Math.random() * passLength)];
+                    }
+                //console.log(rk);
+                while (rj.length < passLength) {
+                    rj +=   par3[Math.floor(Math.random() * passLength)];
+                    }
+            //console.log(rj)
+                while (ry.length < passLength) {
+                    ry +=   par4[Math.floor(Math.random() * passLength)];
+                    }
+            //console.log(ry);
+                while (ru.length < passLength) {
+                    ru +=   par5[Math.floor(Math.random() * passLength)];
+                    }
+                //console.log(ru);
+                document.getElementById('out').innerHTML += '<p>' + rs + '</p>';
+                document.getElementById('out').innerHTML += '<p>' + rk + '</p>';
+                document.getElementById('out').innerHTML += '<p>' + rj + '</p>';
+                document.getElementById('out').innerHTML += '<p>' + ry + '</p>';
+                document.getElementById('out').innerHTML += '<p>' + ru + '</p>';
+                } else{
+                    document.getElementById('out').innerHTML += '<p>' + 'вы не заполнили все поля' + '</p>';
 
-function randomInteger(min, max) {
-    var rand = min - 0.5 + Math.random() * (max - min + 1)
-    rand = Math.round(rand);
-    return rand;
+                }
+  
+/*}else{
+    document.getElementById('out').innerHTML = '';
+}*/
 }
+ function clear(){
+    document.getElementById('out').innerHTML = '';
+ }
